76c6be3e87896b66f21ada56f1472b88541cd809
Marvin Ramirez <eldo@hotmail.com>
2023-06-09 16:01:58 -0400

    fix: added react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>PROJECT 02 - GITHUB DEPLOY TO AWS</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
